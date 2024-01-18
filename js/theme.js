e878c22793cd97a6b54e2237d474cc20f1ba14aa
bhaktiagrawal088 <bhaktiagrawal286@gmail.com>
2024-01-18 20:06:59 +0530

    first commit

---
+++ js/theme.js
@@ -0,0 +1,29 @@
+/**
+ * @copyright codewithsadee 2023
+ * @author sadee <codewithsadee@gmail.com>
+ */
+
+"use strict";
+
+const $HTML /* {NodeElement}*/ = document.documentElement;
+let isDark /*{Boolean} */ = window.matchMedia("(prefers-color-scheme : dark)").matches;
+
+if(sessionStorage.getItem("theme")){
+    $HTML.dataset.theme = sessionStorage.getItem("theme");
+}
+else{
+    $HTML.dataset.theme = isDark ? "dark" : "light";
+}
+
+const changeTheme = function(){
+    console.log("click");
+    isDark = sessionStorage.getItem("theme");
+    sessionStorage.setItem("theme" , isDark == "light" ? "dark" : "light");
+    $HTML.dataset.theme = sessionStorage.getItem("theme");
+}
+
+window.addEventListener('load',()=>{
+    const /* {NodeElemet} */ $themebtn = document.querySelector("[data-theme-toggler]");
+    $themebtn.addEventListener('click', changeTheme);
+})
+
